8482ca9b530d152ef5948a36f7b7bcd375cda9bc
shekharbaj <bimal.sb@gmail.com>
2026-08-27 19:20:35 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="https://shekharbaj.github.io/design-vault-pro/">
+      <BrowserRouter basename="/design-vault-pro/">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/methodology" element={<MethodologyPage />} />
