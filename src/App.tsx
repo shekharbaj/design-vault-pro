cdd4b7da76b7449fdd467c64dde441fb56443bdb
shekharbaj <bimal.sb@gmail.com>
2026-08-27 19:09:44 +0530

    Set basename for BrowserRouter in App component

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/design-vault-pro/">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/methodology" element={<MethodologyPage />} />
